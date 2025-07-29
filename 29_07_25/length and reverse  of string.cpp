f94735ff1fe4ff6d5ab50f6604da3b3ffa94d26e
irfancoding <irfankhan26102006@gmail.com>
2025-07-29 18:06:02 +0530

    Update and rename length of string.cpp to length and reverse  of string.cpp

--- 29_07_25/length and reverse  of string.cpp
+++ 29_07_25/length and reverse  of string.cpp
@@ -1,5 +1,14 @@
-  #include <iostream>
+#include <iostream>
 using namespace std ;
+
+void reverse (char name[], int n){
+    int s=0;
+    int e=n-1;
+    
+    while(s<e){
+        swap(name[s++], name[e--]);
+    }
+}
 int getlength(char name[]) {
     int count =0;
     for(int i=0; name[i] !='\0'; i++){
@@ -17,6 +26,13 @@ int main() {
  cout << "your name is ";
  cout << name<< endl;
  
-cout << " length: " << getlength(name) <<endl;
+ int len = getlength(name);
+ 
+cout << " length: " <<len<<endl;
+reverse(name, len);
+cout << "your name is ";
+cout<< name << endl;
+
+
     return 0;
 }
