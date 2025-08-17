3d6778419152f729a13333b84a2e105f74ba7978
irfancoding <irfankhan26102006@gmail.com>
2025-08-17 15:47:10 +0530

    Create insertion at head and tail node.cpp

---
+++ 2025_08_17/insertion at head and tail node.cpp
@@ -0,0 +1,63 @@
+  #include <iostream>
+using namespace std ;
+
+class node {
+    
+    public :
+    int data ;
+    node* next ;
+    node* prev;
+    
+    node(int d){
+        this -> data = d;
+        this -> next = NULL;
+        this-> prev = NULL;
+    }
+     
+};
+
+ void insertAthead(node* &head, int d) {
+    
+    node* temp = new node(d);
+    temp -> next = head;
+    head -> prev = temp;
+    head = temp;
+}
+  
+  void insertAttail(node* &tail, int d) {
+    
+    node* temp = new node(d);
+    tail -> next = temp;
+    temp -> prev = tail;
+    tail = temp;
+}
+void print (node* head) {
+    node* temp = head ;
+    
+    while (temp != NULL ){
+        cout << temp -> data << " " ;
+        temp = temp -> next;
+    }
+    cout << endl;
+
+}
+
+ int main () {
+    
+    node* node1 = new node(10);
+      node* head = node1;
+      node* tail = node1;
+      
+      insertAthead(head, 11);
+      print(head);
+      
+      insertAthead(head, 16);
+      print(head);
+      
+      insertAttail(tail,28);
+      print(head);
+      
+      
+       
+    return 0;
+}
