0bcd073f4a92f3f8f8fecad191144ce5a528f4bc
irfancoding <irfankhan26102006@gmail.com>
2025-08-18 18:28:08 +0530

    Create circular linked list insertion node.cpp

---
+++ 2025_08_18/circular linked list insertion node.cpp
@@ -0,0 +1,72 @@
+  #include <iostream>
+using namespace std ;
+
+class node {
+    
+    public :
+    int data ;
+    node* next ;
+     
+    
+    node(int d){
+        this -> data = d;
+        this -> next = NULL;
+         
+    }
+    ~node(){
+        int val = this -> data;
+        if(next != NULL){
+            delete next;
+            next = NULL;
+        }
+        cout << " memory free for node with data " << val << endl;
+    }
+     
+};
+ 
+  
+   void insertnode(node* &tail, int element, int d){
+       
+       if(tail == NULL){
+           node* newnode = new node(d);
+           tail = newnode;
+           newnode -> next = newnode;
+       }
+       else {
+           node* curr = tail;
+           
+           while(curr->data !=element){
+               curr = curr -> next;
+           }
+           
+           node* temp = new node(d);
+           temp->next = curr->next;
+           curr-> next = temp;
+       }
+   }
+   void print(node* tail) {
+       
+       node* temp = tail;
+       
+       do{
+           cout << tail-> data << " ";
+           tail = tail -> next;
+       } while(tail != temp);
+            cout << endl ; 
+   }
+   
+int main () {
+    
+    node* tail = NULL;
+     
+     insertnode(tail, 5, 3);
+     print(tail);
+     
+     insertnode(tail, 3, 5);
+     print(tail);
+     
+     insertnode(tail, 5, 7);
+     print(tail);
+     
+    return 0;
+}
