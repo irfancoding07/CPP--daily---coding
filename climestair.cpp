b4891353d63e10235d121634ad8f4ff2762447a6
irfancoding <irfankhan26102006@gmail.com>
2025-09-03 18:19:56 +0530

    Create   climestair.cpp

---
+++ climestair.cpp
@@ -0,0 +1,23 @@
+   #include <iostream>
+using namespace std;
+
+int countDistinctWaytoclimbstair(int n) {
+
+    if(n < 0)
+    return 0;
+    
+    if(n == 0)
+    return 1;
+
+   int ans = countDistinctWaytoclimbstair(n-1) + countDistinctWaytoclimbstair(n-2);
+   
+    return ans;
+}
+
+int main() {
+    int n ;
+    cin >> n;
+    int ans = countDistinctWaytoclimbstair( n);
+    cout << ans << endl;
+    return 0;
+}
