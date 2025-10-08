2298135bbdcb9a77b8ee47f4c7536e6d5912d3ea
irfancoding <irfankhan26102006@gmail.com>
2025-10-08 19:56:37 +0530

    Update min heap using 1-based indexing.cpp

--- 2025_10_08/min heap using 1-based indexing.cpp
+++ 2025_10_08/min heap using 1-based indexing.cpp
@@ -6,11 +6,11 @@ void heapify(int arr[], int n, int i){
     int left = 2*i;
     int right = 2*i + 1;
     
-    if(left < n && arr[smallest] > arr[left]){
+    if(left <= n && arr[smallest] > arr[left]){
         smallest = left;
     }
     
-    if(right < n && arr[smallest] > arr[right]){
+    if(right <= n && arr[smallest] > arr[right]){
         smallest = right;
     }
     if(smallest != i){
