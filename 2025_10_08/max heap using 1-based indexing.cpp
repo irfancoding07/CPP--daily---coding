0e294c6b03ba0412acedc8e75b4d8abf7094298c
irfancoding <irfankhan26102006@gmail.com>
2025-10-08 19:57:08 +0530

    Update max heap using 1-based indexing.cpp

--- 2025_10_08/max heap using 1-based indexing.cpp
+++ 2025_10_08/max heap using 1-based indexing.cpp
@@ -6,11 +6,11 @@ void heapify(int arr[], int n, int i){
     int left = 2*i;
     int right = 2*i + 1;
     
-    if(left < n && arr[largest] < arr[left]){
+    if(left <= n && arr[largest] < arr[left]){
         largest = left;
     }
     
-    if(right < n && arr[largest] < arr[right]){
+    if(right <= n && arr[largest] < arr[right]){
         largest = right;
     }
     if(largest != i){
