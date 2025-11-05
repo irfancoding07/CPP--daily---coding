514bdc370d32c9da1387ddffcd9aa096f750b692
irfancoding <irfankhan26102006@gmail.com>
2025-11-05 20:29:09 +0530

    Create Print a given matrix in spiral form.cpp

---
+++ 2025_11_05/Print a given matrix in spiral form.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int>> &mat) {
+        int row = mat.size();
+        int col = mat[0].size();
+
+        int count = 0;
+        int total = row * col;
+
+        int startingRow = 0;
+        int endingRow = row - 1;
+        int startingCol = 0;
+        int endingCol = col - 1;
+
+        vector<int> ans;
+
+        while (count < total) {
+            
+            // Print top row (→)
+            for (int i = startingCol; count < total && i <= endingCol; i++) {
+                ans.push_back(mat[startingRow][i]);
+                count++;
+            }
+            startingRow++;
+
+            // Print right column (↓)
+            for (int i = startingRow; count < total && i <= endingRow; i++) {
+                ans.push_back(mat[i][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            // Print bottom row (←)
+            for (int i = endingCol; count < total && i >= startingCol; i--) {
+                ans.push_back(mat[endingRow][i]);
+                count++;
+            }
+            endingRow--;
+
+            // Print left column (↑)
+            for (int i = endingRow; count < total && i >= startingRow; i--) {
+                ans.push_back(mat[i][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+
+        return ans;
+    }
+};
+
+ 
+
+int main() {
+    Solution obj;
+
+    vector<vector<int>> mat = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    vector<int> result = obj.spirallyTraverse(mat);
+
+    cout << "Spiral Traversal: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
